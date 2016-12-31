3947dbc8b9b0bf361c53e2fac3941cca9b9a27dd
Anna Heston <amheston@gmail.com>
2016-12-31 10:34:31 -0800

    made all nav bars responsive

--- AnnaWebsite.js
+++ AnnaWebsite.js
@@ -8,3 +8,38 @@ $(document).ready(function() {
 
 });
 
+/*bar chart*/
+
+$(document).ready(function(){
+    barChart();
+    
+    $(window).resize(function(){
+        barChart();
+    });
+    
+    function barChart(){
+        $('.bar-chart').find('.item-progress').each(function(){
+            var itemProgress = $(this),
+            itemProgressWidth = $(this).parent().width() * ($(this).data('percent') / 100);
+            itemProgress.css('width', itemProgressWidth);
+        });
+    };
+});
+
+$(function() {
+  var pull    = $('#pull');
+    menu    = $('nav ul');
+    menuHeight  = menu.height();
+ 
+  $(pull).on('click', function(e) {
+    e.preventDefault();
+    menu.slideToggle();
+  });
+});
+
+$(window).resize(function(){
+  var w = $(window).width();
+  if(w > 320 && menu.is(':hidden')) {
+    menu.removeAttr('style');
+  }
+}); 
